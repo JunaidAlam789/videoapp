83134e5c1f73e613210d82415bd2290fe388c316
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-01-28 23:51:39 +0500

    Update VideoPlayer.jsx

--- app/VideoPlayer.jsx
+++ app/VideoPlayer.jsx
@@ -1,10 +1,12 @@
 "use client"
 import ReactPlayer from "react-player"
+import Link from "next/link"
 export default function VideoPlayer(){
 
 return (
 <div>
-<h1>VideoPlayer</h1>    
+<h1>VideoPlayer</h1>
+<h2><Link href="https://www.youtube.com/watch?v=NB6ZS6US-hc">Ghuftam</Link></h2>
 <ReactPlayer url='https://www.youtube.com/watch?v=NB6ZS6US-hc' />
 </div>
 
