17cd6a8960564b4abc302cb89354192638059091
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-01-28 23:22:58 +0500

    Update VideoPlayer.jsx

--- app/VideoPlayer.jsx
+++ app/VideoPlayer.jsx
@@ -6,6 +6,8 @@ return (
 <div>
 <h1>VideoPlayer</h1>    
 <ReactPlayer url='https://www.youtube.com/watch?v=NB6ZS6US-hc' />
+<br/>
+<ReactPlayer url='https://www.youtube.com/watch?v=XXYcQZI5DZA&t=1381s' />
 </div>
 
 )
