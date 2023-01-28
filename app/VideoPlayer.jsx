4dccb48e652f6aa9f6b5b9da51422b8e4a2e8943
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-01-28 23:06:10 +0500

    Initial commit

---
+++ app/VideoPlayer.jsx
@@ -0,0 +1,13 @@
+"use client"
+import ReactPlayer from "react-player"
+export default function VideoPlayer(){
+
+return (
+<div>
+<h1>VideoPlayer</h1>    
+<ReactPlayer url='https://www.youtube.com/watch?v=NB6ZS6US-hc' />
+</div>
+
+)
+
+}
