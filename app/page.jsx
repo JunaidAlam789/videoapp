d6fbdbca47b5786120c57048bc0e4600d88fe63e
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-01-29 00:29:18 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
-import VideoPlayer from './VideoPlayer'
+//import VideoPlayer from './VideoPlayer'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,7 +10,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-      <VideoPlayer/>
+      <Link href="https://www.youtube.com/watch?v=NB6ZS6US-hc">Ghuftam</Link>
       </div>
     </main>
   )
